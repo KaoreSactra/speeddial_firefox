2ec0ca4939bb70ecd9615f6f355818cfdc9ca4f6
Gustavo Silva <gsilvaaragao@gmail.com>
2025-03-04 15:13:14 -0300

    remove draggable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ export default function Home() {
 
   return (
     <main className="w-dvw h-dvh flex justify-center items-center px-[10dvw] bg-primary">
-      <img src="/img/bg/bg_2.png" className="fixed z-10 w-dvw h-dvh object-cover"/>
+      <img src="/img/bg/bg_2.png" className="fixed z-10 w-dvw h-dvh object-cover select-none pointer-events-none" draggable="false"/>
         <ul className="grid grid-cols-4 gap-10 z-20">
           {containers.map((item) => (
             <li key={item.id} className="rounded-lg hover:scale-106 transition duration-300 ease-in-out shadow-custom">
-              <a href={item.url}>
-                <img src={item.img} alt={item.name} className="rounded-lg"/>
+              <a href={item.url} draggable="false">
+                <img src={item.img} alt={item.name} className="rounded-lg select-none pointer-events-none" draggable="false"/>
               </a>
             </li>
           ))}
