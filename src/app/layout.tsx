50522d8ce8553aed86efb87c76292875c018d330
Gustavo Silva <gsilvaaragao@gmail.com>
2025-03-04 14:14:37 -0300

    custom add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from "next"
+import "./globals.css"
 
 export const metadata: Metadata = {
   title: "Aba Inicial",
   description: "by Gus_Sactra",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="pt-br">
       <body>{children}</body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import containers from "@/json/containers.json"
 
+import Image from 'next/image'
+
 export default function Home() {
 
   return (
-    <main className="w-dvw h-dvh flex justify-center items-center px-[10dvw]">
-        <ul className="grid grid-cols-4 gap-10">
+    <main className="w-dvw h-dvh flex justify-center items-center px-[10dvw] bg-primary">
+      <img src="/img/bg/bg_2.png" className="fixed z-10 w-dvw h-dvh object-cover"/>
+        <ul className="grid grid-cols-4 gap-10 z-20">
           {containers.map((item) => (
-            <li key={item.id} className="hover:scale-106 transition duration-300 ease-in-out">
+            <li key={item.id} className="rounded-lg hover:scale-106 transition duration-300 ease-in-out shadow-custom">
               <a href={item.url}>
                 <img src={item.img} alt={item.name} className="rounded-lg"/>
               </a>
