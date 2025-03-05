15ca889502727b0f7d9514f83129294d6613ce86
Gustavo Silva <gsilvaaragao@gmail.com>
2025-03-04 21:45:00 -0300

    update padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import containers from "@/json/containers.json"
 export default function Home() {
 
   return (
-    <main className="w-dvw h-dvh flex justify-center items-center px-[10dvw] bg-primary">
+    <main className="w-dvw h-dvh flex justify-center items-center px-[14dvw] bg-primary">
       <img src="/img/bg/bg_2.png" className="fixed z-10 w-dvw h-dvh object-cover select-none pointer-events-none" draggable="false"/>
         <ul className="grid grid-cols-4 gap-10 z-20">
           {containers.map((item) => (
